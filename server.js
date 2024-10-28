935bb807b9e11fa86fc148b8ef109b20a2874f31
Min Cheol Song <sendol39@gmail.com>
2024-10-28 09:51:50 +0000

    bind route and auth methods to app

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const { ObjectID } = require('mongodb');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+const routes = require('./routes.js');
+
 
 const app = express();
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
-app.route('/').get((req, res) => {
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+  routes(app, myDataBase);
+  auth(app, myDataBase);
 
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
